558c8c7aee7323ecc4dab72b03bf6e2f01f9b1bf
SNaveenMathew <naveenmathewnathan.s@gmail.com>
2023-12-02 13:53:22 -0500

    Added functions for walk score (working) and transit score (not working, needs a different key)

---
+++ run_first.R
@@ -0,0 +1,75 @@
+#
+# This is the server logic of a Shiny web application. You can run the
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(openxlsx)
+library(walkscoreAPI)
+
+walkscore_API_key <- readLines("walkscore_API_key.txt")
+get_walk_score <- function(address, lat, lon) {
+  url <- paste0("https://api.walkscore.com/score?format=json&address=", address,
+                "&lat=", lat, "&lon=", lon,
+                "&wsapikey=", walkscore_API_key)
+  return(httr::GET(url, httr::accept_json()))
+}
+
+get_transit_score <- function(lat, lon, city, state) {
+  url <- paste0("https://transit.walkscore.com/transit/score/?lat=", lat,
+                "&lon=", lon, "&city=", city, "&state=", state,
+                "&wsapikey=", walkscore_API_key)
+  return(httr::GET(url, httr::accept_json()))
+}
+
+first_element <- function(x) {
+  return(x[1])
+}
+
+redfin_data <- openxlsx::read.xlsx(xlsxFile = "Data/Apartment.xlsx", sheet = "Consolidated", startRow = 2)
+redfin_data$Address_Only <- sapply(strsplit(
+  sapply(strsplit(x = gsub(
+    x = redfin_data$Address,
+    pattern = "\\.",
+    replacement = " "),
+    split = " Unit"), first_element
+  ), split = " \\#"), first_element
+)
+
+redfin_data$Address_for_URL <- gsub(
+  x = paste(
+    redfin_data$Address_Only,
+    redfin_data$City,
+    redfin_data$State
+  ),
+  pattern = " ",
+  replacement = "%20"
+)
+
+# Walk score
+res <- walkscoreAPI::getWS(redfin_data$Longitude[3], redfin_data$Latitude[3], walkscore_API_key)
+res$walkscore
+res <- get_walk_score(address = redfin_data$Address_for_URL[3], lat = redfin_data$Latitude[3], lon = redfin_data$Longitude[3])
+httr::content(res)
+
+for(i in 1:nrow(redfin_data)) {
+  if(is.na(redfin_data$Walk.score[i])) {
+    res <- walkscoreAPI::getWS(redfin_data$Longitude[i], redfin_data$Latitude[i], walkscore_API_key)
+    redfin_data$Walk.score[i] <- res$walkscore
+  }
+}
+
+# Transit score
+# res <- walkscoreAPI::getTS(redfin_data$Longitude[3], redfin_data$Latitude[3],
+#                            redfin_data$City[3], state = redfin_data$State[3],
+#                            walkscore_API_key)
+# res <- get_transit_score(lat = redfin_data$Latitude[3], lon = redfin_data$Longitude[3],
+#                          city = redfin_data$City[3], state = redfin_data$State[3])
+# httr::content(res)
+
+# Other
+# walkscoreAPI::networkSearch(redfin_data$Longitude[3], redfin_data$Latitude[3], walkscore_API_key)
+# walkscoreAPI::walkshed(redfin_data$Longitude[3], redfin_data$Latitude[3], walkscore_API_key)
